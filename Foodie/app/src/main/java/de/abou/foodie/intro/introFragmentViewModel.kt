234f40ea6687f8a2c618a2aca20f37b45f1246d5
ahmedAbouismail <ahmed.abouismail96@gmail.com>
2020-12-08 21:04:37 +0100

    Update SignIn and SignUp

--- Foodie/app/src/main/java/de/abou/foodie/intro/introFragmentViewModel.kt
+++
@@ -1,6 +0,0 @@
-package de.abou.foodie.intro
-
-import androidx.lifecycle.ViewModel
-
-class introFragmentViewModel:ViewModel() {
-}
--- Foodie/app/src/main/java/de/abou/foodie/models/User.kt
+++
@@ -1,9 +0,0 @@
-package de.abou.foodie.models
-
-data class User(val id :String = "",
-val name: String = "",
-val email:String = "",
-val image:String = "",
-val mobile:Long = 0,
-val fcmToken:String ="") {
-}
